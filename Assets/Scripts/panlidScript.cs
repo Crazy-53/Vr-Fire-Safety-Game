5d9aa17f0d54687d311a791a6a5db72d7f3cdc03
Taha Ghazi <tahaghazt164@gmail.com>
2025-12-13 15:46:58 +0200

    Enable gravity and proper physics on pan lid - Added Rigidbody to lid - Configured Box Collider for correct collisions - Set collision detection and interpolation for stability

--- Assets/Scripts/panlidScript.cs
+++ Assets/Scripts/panlidScript.cs
@@ -1,10 +1,61 @@
-
 using UnityEngine;
 
-public class panlidScript : MonoBehaviour
+public class LidGrab : MonoBehaviour
 {
-    private void Onparticalecollision(GameObject other)
+    public Transform grabPoint; // where the lid will attach when grabbed
+    public KeyCode grabKey = KeyCode.K; // key to grab/release
+
+    private bool playerNearby = false;
+    private bool isGrabbed = false;
+    private Transform originalParent;
+    private Rigidbody rb;
+
+    void Start()
     {
-        Debug.Log("collide");
+        originalParent = transform.parent;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(grabKey))
+        {
+            if (!isGrabbed)
+            {
+                Grab();
+            }
+            else
+            {
+                Release();
+            }
+        }
+    }
+
+    void Grab()
+    {
+        isGrabbed = true;
+        rb.isKinematic = true; // disable physics while holding
+        transform.position = grabPoint.position;
+        transform.rotation = grabPoint.rotation;
+        transform.SetParent(grabPoint);
+    }
+
+    void Release()
+    {
+        isGrabbed = false;
+        rb.isKinematic = false;
+        transform.SetParent(originalParent);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerNearby = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerNearby = false;
     }
 }
